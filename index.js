505a4a1791dcddae3bf5d8389fe729c05c8750e5
Noelle <noelle.maingi@student.moringaschool.com>
2023-06-15 22:53:04 +0300

    Compeleted Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function beforeEach(){
+    console.log(cats);
+}
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newName = [...cats, name ]
+    return newName;
+
+}
+
+function prependCat(name){
+    const newName = [name, ...cats]
+    return newName;
+}
+
+function removeLastCat(){
+    const newName = cats.slice(0,-1);
+    return newName;
+}
+function removeFirstCat(){
+    const newName = cats.slice(1, 3);
+    return newName;
+}
